9702c11c539e6d755ff252c8992d7b19a304fcc8
PhuongBT337 <b.t.phuong337@gmail.com>
2025-03-24 21:37:38 +0900

    update bai tap 02

---
+++ Ex02/Ex02.js
@@ -0,0 +1,13 @@
+let i= 10;
+let f= 20.5;
+let b= true;
+let s= "Hà Nội";
+
+
+document.writeln(i);
+document.writeln("</br>");
+document.writeln(f);
+document.writeln("</br>");
+document.writeln(b);
+document.writeln("</br>");
+document.writeln(s);
