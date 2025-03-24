ac977cd0a949cabd987b4df8a9655830889d5e00
PhuongBT337 <b.t.phuong337@gmail.com>
2025-03-24 22:48:17 +0900

    update bai tap 06

---
+++ Ex06/Ex06.js
@@ -0,0 +1,7 @@
+let math= parseFloat(prompt('Nhap diem toan: '));
+let physics= parseFloat(prompt('Nhap diem ly: '));
+let chemistry= parseFloat(prompt('Nhap diem hoa: '));
+
+let dtb= (math+physics+chemistry)/3;
+
+console.log('Diem trung binh la: ' + dtb);
